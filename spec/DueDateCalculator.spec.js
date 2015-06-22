5d698062241397ff2d162fe1c3310f4eabb8488b
stoiet <to.stoiet@gmail.com>
2015-06-22 11:46:21 +0200

    Implement test case with not working day given as submit day

--- spec/DueDateCalculator.spec.js
+++ spec/DueDateCalculator.spec.js
@@ -12,7 +12,15 @@ describe("DueDateCalculator", function () {
             expect(duedateCalculator.calculateDueDate.bind(duedateCalculator, {
                 submitDate: new Date('June 22, 2015 18:00:00'),
                 turnaroundHours: 0
-            })).to.throw('Submit date is not a working day!');
+            })).to.throw('Invalid submit date!');
+        });
+
+        it("should throw an exception when not working hours given", function () {
+            var duedateCalculator = new DueDateCalculator();
+            expect(duedateCalculator.calculateDueDate.bind(duedateCalculator, {
+                submitDate: new Date('June 27, 2015 09:00:00'),
+                turnaroundHours: 0
+            })).to.throw('Invalid submit date!');
         });
 
     });
--- src/DueDateCalculator.js
+++ src/DueDateCalculator.js
@@ -5,10 +5,16 @@ function DueDateCalculator () {}
 DueDateCalculator.prototype = {
     calculateDueDate : function (problemDateParameters) {
         if (!this._isWorkingHours(problemDateParameters.submitDate))
-            throw new Error('Submit date is not a working day!');
+            throw new Error('Invalid submit date!');
+
+        if (!this._isWorkingDay(problemDateParameters.submitDate))
+            throw new Error('Invalid submit date!');
     },
     _isWorkingHours: function (submitDate) {
         return (submitDate.getHours() > 8) && (submitDate.getHours() < 17);
+    },
+    _isWorkingDay: function (submitDate) {
+        return (submitDate.getDay() > 0) && (submitDate.getDay() < 6);
     }
 };
 
